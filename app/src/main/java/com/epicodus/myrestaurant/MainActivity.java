45b5747287f8de611093659ba5c9e821e385687e
Kimberly Lu <kimlu4@comcast.net>
2017-10-07 20:16:35 -0700

    continue with test using robolectric and expresso.

--- app/src/main/java/com/epicodus/myrestaurant/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurant/MainActivity.java
@@ -1,24 +1,30 @@
 package com.epicodus.myrestaurant;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = MainActivity.class.getSimpleName();
-    private Button mFindRestaurantButton;
-    private EditText mLocationEditText;
+    @Bind(R.id.findRestaurantButton) Button mFindRestaurantButton;
+    @Bind(R.id.locationEditText) EditText mLocationEditText;
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
-        mFindRestaurantButton = (Button) findViewById(R.id.findRestaurantButton);
+        ButterKnife.bind(this);
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
+        mAppNameTextView.setTypeface(ostrichFont);
+
         mFindRestaurantButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
